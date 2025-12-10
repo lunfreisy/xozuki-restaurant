c6aebbf1d5af80fc1baa6ac93ca27ce345a372d2
Freisy <lun.freisy@gmail.com>
2025-12-10 23:10:47 +0300

    Добавлена секция меню

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// дождь
 const rainContainer = document.querySelector('.rain-container');
 const dropsCount = 100;
 
@@ -13,3 +14,48 @@ drop.style.animationDelay = (Math.random() * 2) + 's';
 
 rainContainer.appendChild(drop);
 }
+
+  // меню
+document.addEventListener('DOMContentLoaded', () => {
+  const cards = document.querySelectorAll('.menu-card');
+  const prevBtn = document.querySelector('.prev-btn');
+  const nextBtn = document.querySelector('.next-btn');
+  let currentIndex = 0;
+
+  function showCard(index) {
+    cards.forEach((card, i) => {
+      card.classList.toggle('active', i === index);
+    });
+  }
+
+  nextBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % cards.length;
+    showCard(currentIndex);
+  });
+
+  prevBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + cards.length) % cards.length;
+    showCard(currentIndex);
+  });
+
+  showCard(currentIndex);
+});
+
+  // открытие и закрытие модалки
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.btn[data-target]').forEach(btn => {
+    btn.addEventListener('click', (e) => {
+      const targetId = btn.getAttribute('data-target');
+      const modal = document.getElementById(targetId);
+      if(modal) modal.classList.add('show'); // показываем модалку
+    });
+  });
+
+  document.querySelectorAll('.close-btn').forEach(btn => {
+    btn.addEventListener('click', (e) => {
+      const modal = btn.closest('.menu-modal');
+      if(modal) modal.classList.remove('show');
+    });
+  });
+});
+
